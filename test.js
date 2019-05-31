c0ae639637143113f3eaf19acbee62774bca8e36
maksstinger <36172633+maksstinger@users.noreply.github.com>
2019-05-31 11:12:24 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ const sum = require('./src/main');
 describe("Check right sum", function() {
 
     it("sums two args", function() {
-        const actual = sum(3)(4);
-        assert.equal(actual, 7);
+        const result = sum(3)(4);
+        assert.equal(result, 7);
     });
     it("sums three args", function() {
-        const actual = sum(3)(4)(11);
-        assert.equal(actual, 18);
+        const result = sum(3)(4)(11);
+        assert.equal(result, 18);
     });
     it("sums six args", function() {
-        const actual = sum(3)(3)(-1)(10)(1)(0);
-        assert.equal(actual, 16);
+        const result = sum(3)(3)(-1)(10)(1)(0);
+        assert.equal(result, 16);
     });
 });
